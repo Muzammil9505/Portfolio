b9270f48b795d7ef89933e8a81ee912304052f14
SHAIK MOHAMMED MUZAMMIL <mzlshaik9505@gmail.com>
2023-08-03 00:05:11 +0530

    Portfolio

---
+++ Portfolio Responsive/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Set the body visibility to "visible" once the page is fully loaded
+    document.body.style.visibility = "visible";
+  });
+
+ 
